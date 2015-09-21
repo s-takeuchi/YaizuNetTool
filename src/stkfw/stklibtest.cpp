99e4c0d6ce4cca9c3438423eeffac1f017e4ea82
Shinya Takeuchi <bamboo@rj.dream.jp>
2015-09-21 16:21:39 +0900

    #8 Change history information has been added for this enhancement.

--- src/stkfw/stklibtest.cpp
+++ src/stkfw/stklibtest.cpp
@@ -810,7 +810,7 @@ int SetAboutInfo()
 	AddHistoryItem(1, _T("1068"), _T("1.4.0"), MyMsgProc::GetMsg(MyMsgProc::SFW_130_140_3));
 	AddHistoryItem(1, _T("1073"), _T("1.4.0"), MyMsgProc::GetMsg(MyMsgProc::SFW_130_140_6));
 	AddHistoryItem(1, _T("2003"), _T("1.5.0"), MyMsgProc::GetMsg(MyMsgProc::SFW_140_150_1));
-	AddHistoryItem(1, _T("2003"), _T("1.5.0"), MyMsgProc::GetMsg(MyMsgProc::SFW_140_150_2));
+	AddHistoryItem(1, _T("2008"), _T("1.5.0"), MyMsgProc::GetMsg(MyMsgProc::SFW_140_150_2));
 
 	return 0;
 }
