18d078f0ae8dca7471ef36eafa2f4c33b8672df5
Shinya Takeuchi <sy-takeuchi@outlook.com>
2018-10-14 16:28:48 +0900

    #41 : Code optimization

--- src/stkfw/HttpHeader.cpp
+++ src/stkfw/HttpHeader.cpp
@@ -19,11 +19,19 @@ bool HttpHdDateFlag = false;
 HWND HttpHdRequest = NULL;
 bool HttpHdRequestFlag = false;
 HWND HttpHdResponse = NULL;
-bool HttpHdResponseFlag = false;
-HWND HttpHttpHeaderEd = NULL;
+HWND HttpHeaderEd = NULL;
+
+wchar_t HttpHeaderEdBox[1024] = L"";
 
 void ChangeHttpHeader()
 {
+	if (HttpHdDelHttpFlag == true) {
+		SendMessage(HttpHdDelHttp, BM_SETCHECK, BST_CHECKED, 0L);
+		EnableWindow(HttpHdDelHttp, true);
+	} else {
+		SendMessage(HttpHdDelHttp, BM_SETCHECK, BST_UNCHECKED, 0L);
+		EnableWindow(HttpHdDelHttp, true);
+	}
 	bool HttpHdFlag = false;
 	if (HttpHdAddHttpFlag == false) {
 		HttpHdFlag = false;
@@ -38,11 +46,11 @@ void ChangeHttpHeader()
 	EnableWindow(HttpHdDate, HttpHdFlag);
 	EnableWindow(HttpHdRequest, HttpHdFlag);
 	EnableWindow(HttpHdResponse, HttpHdFlag);
-	EnableWindow(HttpHttpHeaderEd, HttpHdFlag);
+	EnableWindow(HttpHeaderEd, HttpHdFlag);
 	SendMessage(HttpHdContentLen, BM_SETCHECK, (HttpHdContentLenFlag == false) ? BST_UNCHECKED : BST_CHECKED, 0L);
 	SendMessage(HttpHdDate, BM_SETCHECK, (HttpHdDateFlag == false) ? BST_UNCHECKED : BST_CHECKED, 0L);
 	SendMessage(HttpHdRequest, BM_SETCHECK, (HttpHdRequestFlag == false) ? BST_UNCHECKED : BST_CHECKED, 0L);
-	SendMessage(HttpHdResponse, BM_SETCHECK, (HttpHdResponseFlag == false) ? BST_UNCHECKED : BST_CHECKED, 0L);
+	SendMessage(HttpHdResponse, BM_SETCHECK, (HttpHdRequestFlag == true) ? BST_UNCHECKED : BST_CHECKED, 0L);
 }
 
 void HttpHeader(int CurrentId, int Type, HINSTANCE InstHndl, HWND WndHndl, UINT message, WPARAM wParam, LPARAM lParam)
@@ -67,15 +75,18 @@ void HttpHeader(int CurrentId, int Type, HINSTANCE InstHndl, HWND WndHndl, UINT 
 			GetMsgWidth(WndHndl, MyMsgProc::GetMsg(MyMsgProc::PROP_HTTPHEADER_DATE)) + 30,
 			GetMsgHeight(WndHndl, MyMsgProc::GetMsg(MyMsgProc::PROP_HTTPHEADER_DATE)),
 			WndHndl, (HMENU)IDC_HTTPHD_DATE, InstHndl, NULL);
-		HttpHdRequest = CreateWindow(_T("BUTTON"), MyMsgProc::GetMsg(MyMsgProc::PROP_HTTPHEADER_REQUEST), WS_CHILD | WS_VISIBLE | BS_CHECKBOX, 40, 220,
+		HttpHdRequest = CreateWindow(_T("BUTTON"), MyMsgProc::GetMsg(MyMsgProc::PROP_HTTPHEADER_REQUEST), WS_CHILD | WS_VISIBLE | BS_RADIOBUTTON, 40, 220,
 			GetMsgWidth(WndHndl, MyMsgProc::GetMsg(MyMsgProc::PROP_HTTPHEADER_REQUEST)) + 30,
 			GetMsgHeight(WndHndl, MyMsgProc::GetMsg(MyMsgProc::PROP_HTTPHEADER_REQUEST)),
 			WndHndl, (HMENU)IDC_HTTPHD_REQUEST, InstHndl, NULL);
-		HttpHdResponse = CreateWindow(_T("BUTTON"), MyMsgProc::GetMsg(MyMsgProc::PROP_HTTPHEADER_RESPONSE), WS_CHILD | WS_VISIBLE | BS_CHECKBOX, 40, 250,
+		HttpHdResponse = CreateWindow(_T("BUTTON"), MyMsgProc::GetMsg(MyMsgProc::PROP_HTTPHEADER_RESPONSE), WS_CHILD | WS_VISIBLE | BS_RADIOBUTTON, 40, 250,
 			GetMsgWidth(WndHndl, MyMsgProc::GetMsg(MyMsgProc::PROP_HTTPHEADER_RESPONSE)) + 30,
 			GetMsgHeight(WndHndl, MyMsgProc::GetMsg(MyMsgProc::PROP_HTTPHEADER_RESPONSE)),
 			WndHndl, (HMENU)IDC_HTTPHD_RESPONSE, InstHndl, NULL);
-		HttpHttpHeaderEd = CreateWindowEx(WS_EX_CLIENTEDGE, _T("EDIT"), _T(""), WS_CHILD | WS_VISIBLE | ES_AUTOHSCROLL, Rect.left + 40, 280, Rect.right - 50, 210, WndHndl, NULL, InstHndl, NULL);
+		HttpHeaderEd = CreateWindowEx(WS_EX_CLIENTEDGE, _T("EDIT"), _T(""), WS_CHILD | WS_VISIBLE | ES_AUTOHSCROLL, Rect.left + 40, 280, Rect.right - 50, 210, WndHndl, NULL, InstHndl, NULL);
+		SendMessage(HttpHeaderEd, EM_SETLIMITTEXT, (WPARAM)1024, (LPARAM)0);
+		SendMessage(HttpHeaderEd, WM_SETTEXT, (WPARAM)0, (LPARAM)HttpHeaderEdBox);
+
 		ChangeHttpHeader();
 	}
 	if (message == WM_COMMAND) {
@@ -83,12 +94,8 @@ void HttpHeader(int CurrentId, int Type, HINSTANCE InstHndl, HWND WndHndl, UINT 
 			if (LOWORD(wParam) == IDC_HTTPHD_DELETE) {
 				if (HttpHdDelHttpFlag == false) {
 					HttpHdDelHttpFlag = true;
-					SendMessage(HttpHdDelHttp, BM_SETCHECK, BST_CHECKED, 0L);
-					EnableWindow(HttpHdDelHttp, true);
 				} else {
 					HttpHdDelHttpFlag = false;
-					SendMessage(HttpHdDelHttp, BM_SETCHECK, BST_UNCHECKED, 0L);
-					EnableWindow(HttpHdDelHttp, true);
 				}
 			}
 			if (LOWORD(wParam) == IDC_HTTPHD_ADD) {
@@ -113,18 +120,10 @@ void HttpHeader(int CurrentId, int Type, HINSTANCE InstHndl, HWND WndHndl, UINT 
 				}
 			}
 			if (LOWORD(wParam) == IDC_HTTPHD_REQUEST) {
-				if (HttpHdRequestFlag == false) {
-					HttpHdRequestFlag = true;
-				} else {
-					HttpHdRequestFlag = false;
-				}
+				HttpHdRequestFlag = true;
 			}
 			if (LOWORD(wParam) == IDC_HTTPHD_RESPONSE) {
-				if (HttpHdResponseFlag == false) {
-					HttpHdResponseFlag = true;
-				} else {
-					HttpHdResponseFlag = false;
-				}
+				HttpHdRequestFlag = false;
 			}
 			ChangeHttpHeader();
 		}
